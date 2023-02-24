1a68f5eb4af742c0071b2ab96612200c898779bc
lucas_finoti <lucasfinotirodrigues@hotmail.com>
2023-02-24 16:56:43 -0300

    feat: Finalizando a primeira tarefa do curso

---
+++ Segundo-Projeto/Primeira-Tarefa/programa.js
@@ -0,0 +1,10 @@
+var numero1 = 8;
+var numero2 = 2;
+
+console.log("A soma entre os números", numero1, "com  o", numero2, "resulta em:", numero1 + numero2);
+
+console.log("A subtração entre os números," ,numero1, "com o", numero2, "resulta em:", numero1 - numero2);
+
+console.log("A Divisão entre os números," ,numero1, "com o", numero2, "resulta em:", numero1 / numero2);
+
+console.log("A Multiplicação entre os números," ,numero1, "com o", numero2, "resulta em:", numero1 * numero2);
